674329e7f7504b550727b19dbb0ea232a073c6f5
peter <petermbaik@gmail.com>
2019-12-09 11:33:50 +0300

    injected our services in our component

--- src/app/git-search/searched.service.ts
+++ src/app/git-search/searched.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import 'rxjs/add/operator/map';
+import {HttpClientModule} from '@angular/common/http'
+import "rxjs/add/operator/map";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,16 @@ export class SearchedService {
 private username:string
 private clientid:'5bc8503b88f0083c5559'
 private clientsecret:'d28bbc6426e9c7aa507f8a61d9905e6863480bf7'
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClientModule) { 
     console.log('i am ready')
     this.username="kuria"
   }
 
   showProfile(){
-    this.http.get("https://api.github.com/users/" + this.username + "?client_id=" +this.clientid + "&client_secret=" + 
-    this.clientsecret).map(res=>res.json());
+    
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" +this.clientid + "&client_secret=" + 
+    this.clientsecret).map(res=>res.json())
+    
+    };
   }
 }
--- src/app/home-search/home-search.component.ts
+++ src/app/home-search/home-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SearchedService } from '../git-search/searched.service';
 @Component({
   selector: 'app-home-search',
   templateUrl: './home-search.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchservice:SearchedService) {
+    this.searchservice.showProfile().subscribe(profile=>{
+      console.log(profile)
+    })
+   }
 
   ngOnInit() {
   }
