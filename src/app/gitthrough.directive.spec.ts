c4b9d2519601a165fca234bdc8ae6630142d9f0a
peter <petermbaik@gmail.com>
2019-12-10 08:53:55 +0300

    Auto-generated commit

--- src/app/gitthrough.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GitthroughDirective } from './gitthrough.directive';
-
-describe('GitthroughDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GitthroughDirective();
-    expect(directive).toBeTruthy();
-  });
-});
