0c139f4256a638380577b4f85453f9db8028fd91
peter <petermbaik@gmail.com>
2019-12-10 06:57:23 +0300

    added a custom directive

--- src/app/gitthrough.directive.ts
+++ src/app/gitthrough.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive ,ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appGitthrough]'
 })
 export class GitthroughDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { this.elem.nativeElement.style.textDecoration='line-through';}
 
 }
